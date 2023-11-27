ea5e0b13935e01717dc7c18f0ae6306b81d3f4bb
Łukasz Markowski <lucas20193@gmail.com>
2023-11-27 11:00:02 +0100

    [TASK] [27.11.2023]

--- tasks/2023-11-27/index.ts
+++ tasks/2023-11-27/index.ts
@@ -1,1 +1,13 @@
-// Tutaj skopiuj kod zadania
+export function calculateTilesNeeded(
+  sections: Array<{ width: number; height: number }>
+): number {
+  let totalTiles = 0;
+
+  if (sections.length > 0) {
+    totalTiles = sections
+      .map((section) => section.width * section.height)
+      .reduce((acc, cur) => acc + cur);
+  }
+
+  return totalTiles;
+}
